03c2e977718ca2238fac44a853c1009ff39913dd
“Foxshane997” <Foxshane997@gmail.com>
2024-04-03 14:57:57 -0700

    adding chagnes

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,16 @@
 
 // Your Code Here
+
+// let response = await fetch ('http://localhost:3001/updateBook', {
+//     method: "PATCH",
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body:JSON.stringify({
+//         "id": 3,
+//         "title": "Legends of Arathrae",
+//     }),
+// });
+// let updateBook = await response.json();
+// console.log(updateBook)
+
