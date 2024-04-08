0ff85de39f9a11d80bac3f96acbd30f006380a7a
“Foxshane997” <Foxshane997@gmail.com>
2024-04-07 17:33:14 -0700

    adding changes

--- public/admin.js
+++ public/admin.js
@@ -19,18 +19,42 @@
 // end of part 2
 
 /* start of part 3 */
+console.log("hi"); /* testing log */
 
-async function main() {
+async function main () {
     let response = await fetch('http://localhost:3001/listBooks');
-
     let books = await response.json();
-
-    books.foreach(renderBook);
+    books.forEach(renderBook);
 }
 
-function renderBook(book){
-    let bookContainer = document.querySelector('.book-container')
-    bookContainer.innerHTML +=`
-    <div class>
-    `
+function renderBook(book) {
+    let root = document.querySelector('#root');
+
+    let li = document.createElement('li');
+    li.textContent = book.title;
+
+    let quantityInput = document.createElement('input');
+    quantityInput.value = book.quantity;
+
+    let saveButton = document.createElement('button');
+    saveButton.textContent = 'save';
+
+    saveButton.addEventListener('click', () =>{
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: quantityInput.value
+            })
+        })
+    })
+
+    li.append(quantityInput, saveButton);
+
+    root.append(li);
 }
+
+main();
